9411bf133046d86ff323be1a9202fdf94e42724f
Jon Robison <narfman0@gmail.com>
2020-10-18 22:39:14 -0400

    -Add npc unit test. Its all pretty trivial but good practice and better to be there.

---
+++ tests/test_npc.py
@@ -0,0 +1,23 @@
+import unittest
+
+import pytest
+
+from abbot.npc import NPC
+
+
+def test_npc_attack():
+    attacker = NPC("kingkrool")
+    defender = NPC("kingkrool")
+    attacker.attack([defender])
+    assert defender.current_hp < defender.hp
+
+
+def test_npc_fainted():
+    npc = NPC("kingkrool")
+    npc.current_hp -= npc.current_hp
+    assert npc.fainted()
+
+
+def test_npc_updated():
+    npc = NPC("kingkrool")
+    npc.update()
