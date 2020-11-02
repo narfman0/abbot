e297da1cc37d7f290924149066f6e120b87c874d
Jon Robison <narfman0@gmail.com>
2020-11-01 23:00:31 -0500

    -Split out driver and ui window. I foresee some hot camera action soon!

---
+++ abbot/driver.py
@@ -0,0 +1,86 @@
+import random
+from typing import Optional
+
+import arcade
+import pymunk
+
+from abbot.math import distance
+from abbot.npc import NPC, ATTACK_DISTANCE
+from abbot.galaxy import Chunk, Galaxy
+
+# Movement speed of player, in pixels per frame
+MOVEMENT_SPEED = 2
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 25
+PLAYER_MOVE_FORCE_ON_GROUND = 1000
+
+
+class Driver():
+    def __init__(self):
+        # Set up the player, specifically placing it at these coordinates.
+        self.player = NPC("kingkrool", hp=100)
+
+        self.moving_left = False
+        self.moving_right = False
+        self.moving_up = False
+        self.moving_down = False
+
+        self.do_attack = False
+        self.galaxy = Galaxy()
+
+        # Physics
+        self.space = pymunk.Space()
+        self.space.add(self.player.body, self.player.poly)
+        self.active_chunks = []
+        self.active_chunks = self.update_active_chunks()
+
+    def update(self, delta_time):
+        """ Movement and game logic """
+        if self.moving_up:
+            force = (0, PLAYER_MOVE_FORCE_ON_GROUND)
+            self.player.body.apply_force_at_local_point(force, (0, 0))
+        if self.moving_down:
+            force = (0, -PLAYER_MOVE_FORCE_ON_GROUND)
+            self.player.body.apply_force_at_local_point(force, (0, 0))
+        if self.moving_left:
+            force = (-PLAYER_MOVE_FORCE_ON_GROUND, 0)
+            self.player.body.apply_force_at_local_point(force, (0, 0))
+        if self.moving_right:
+            force = (PLAYER_MOVE_FORCE_ON_GROUND, 0)
+            self.player.body.apply_force_at_local_point(force, (0, 0))
+        self.player.update()
+
+        self.update_active_chunks()
+        self.space.step(delta_time)
+        if self.do_attack:
+            self.do_attack = False
+            self.player.attack([])
+
+    def update_active_chunks(self):
+        last_active_chunks = self.active_chunks or []
+        self.active_chunks = list(
+            self.galaxy.position_to_active_chunks(
+                self.player.x, self.player.y
+            )
+        )
+        # remove chunk sprites from engine
+        for last_active_chunk in last_active_chunks:
+            if not last_active_chunk in self.active_chunks:
+                for celestial_body in last_active_chunk.celestial_bodies:
+                    self.space.remove(celestial_body.shape, celestial_body.body)
+                    print(f"Removed celestial_body {celestial_body.center_x},{celestial_body.center_y}")
+                print(f"Removed chunk {last_active_chunk.chunk_x},{last_active_chunk.chunk_y}")
+        # add chunk sprites to engine
+        for active_chunk in self.active_chunks:
+            if not active_chunk in last_active_chunks:
+                for celestial_body in active_chunk.celestial_bodies:
+                    mass = 1
+                    moment = pymunk.moment_for_circle(mass, 0, celestial_body.radius) 
+                    body = pymunk.Body(mass, moment, body_type=pymunk.body.Body.STATIC)
+                    body.position = celestial_body.center_x, celestial_body.center_y
+                    shape = pymunk.Circle(body, celestial_body.radius)
+                    self.space.add(body, shape)
+                    celestial_body.body = body
+                    celestial_body.shape = shape
+                    print(f"Created celestial body at {celestial_body.center_x},{celestial_body.center_y} with r={celestial_body.radius}")
+                print(f"Added chunk {active_chunk.chunk_x},{active_chunk.chunk_y}")
---
+++ abbot/ui/gameplay_window.py
@@ -0,0 +1,95 @@
+import random
+from typing import Optional
+
+import arcade
+import pymunk
+
+from abbot.math import distance
+from abbot.npc import NPC, ATTACK_DISTANCE
+from abbot.driver import Driver
+
+SCREEN_TITLE = "Abbot"
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 1024
+
+
+class GameplayWindow(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        self.view_left = -SCREEN_WIDTH//2
+        self.view_bottom = -SCREEN_HEIGHT//2
+        self.driver = Driver()
+
+    def on_draw(self):
+        """ Render the screen. """
+        # viewport and camera
+        self.view_left = int(self.driver.player.x) - SCREEN_WIDTH // 2
+        self.view_bottom = int(self.driver.player.y) - SCREEN_HEIGHT // 2
+        arcade.set_viewport(
+            self.view_left,
+            SCREEN_WIDTH + self.view_left,
+            self.view_bottom,
+            SCREEN_HEIGHT + self.view_bottom,
+        )
+
+        # Clear the screen to the background color
+        arcade.start_render()
+
+        if not self.driver.player.fainted():
+            self.driver.player.draw()
+
+        for chunk in self.driver.active_chunks:
+            for celestial_body in chunk.celestial_bodies:
+                arcade.draw_circle_filled(
+                    celestial_body.center_x,
+                    celestial_body.center_y,
+                    celestial_body.radius,
+                    arcade.color.YELLOW,
+                )
+
+        # Draw our score on the screen, scrolling it with the viewport
+        score_text = f"{self.driver.player.x:.2f},{self.driver.player.y:.2f} angle: {self.driver.player.angle:.2f} hp: {self.driver.player.current_hp}"
+        arcade.draw_text(
+            score_text,
+            self.view_left + 10,
+            self.view_bottom + 10,
+            arcade.csscolor.WHITE,
+            18,
+        )
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+        self.driver.update(delta_time)
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        if key == arcade.key.R:
+            self.driver = Driver()
+            return
+        if not self.driver.player.fainted():
+            if key == arcade.key.UP:
+                self.driver.moving_up = True
+            if key == arcade.key.LEFT:
+                self.driver.moving_left = True
+            if key == arcade.key.RIGHT:
+                self.driver.moving_right = True
+            if key == arcade.key.DOWN:
+                self.driver.moving_down = True
+            if key == arcade.key.A:
+                self.driver.do_attack = True
+            if key == arcade.key.C:
+                self.driver.player.body.angle = 0
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+        if key == arcade.key.UP:
+            self.driver.moving_up = False
+        if key == arcade.key.LEFT:
+            self.driver.moving_left = False
+        if key == arcade.key.RIGHT:
+            self.driver.moving_right = False
+        if key == arcade.key.DOWN:
+            self.driver.moving_down = False
